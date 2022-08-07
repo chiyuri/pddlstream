dbed5bc876fc3ae2c0cd0e60164cf64172830272
Chiyuri <72197444+chiyuri@users.noreply.github.com>
2022-08-07 15:42:33 +0100

    v310 update

--- pddlstream/language/stream.py
+++ pddlstream/language/stream.py
@@ -1,6 +1,6 @@
 import time
-from collections import Counter, Sequence
-
+from collections import Counter
+from collections.abc import Sequence
 from pddlstream.algorithms.common import INTERNAL_EVALUATION, add_fact
 from pddlstream.algorithms.downward import make_axiom
 from pddlstream.language.constants import AND, get_prefix, get_args, is_parameter, Fact, concatenate, StreamAction, Output
